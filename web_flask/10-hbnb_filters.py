5d4c0c5d8a04f7de920c161b83e56e9f01bccccf
Omar J <omarj819@gmail.com>
2024-04-10 15:18:04 +0300

    A

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -3,38 +3,23 @@
 Script to start a Flask web application
 """
 
-from flask import Flask, render_template # type: ignore
+from flask import Flask, render_template
 from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-
-@app.route('/hbnb_filters', strict_slashes=False)
+@app.route('/hbnb_filters')
 def hbnb_filters():
-    """
-    Route to display a HTML page like 6-index.html
-    """
-    states = storage.all(State).values()
-    cities = storage.all(City).values()
-    amenities = storage.all(Amenity).values()
-    return render_template(
-        '10-hbnb_filters.html',
-        states=states,
-        cities=cities,
-        amenities=amenities
-    )
-
+    states = storage.all("State").values()
+    states.sort(key=lambda x: x.name)
+    amenities = storage.all("Amenity").values()
+    amenities.sort(key=lambda x: x.name)
+    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
 
 @app.teardown_appcontext
-def teardown(exception):
-    """
-    Teardown method to remove the current SQLAlchemy Session
-    """
+def teardown_db(exception=None):
     storage.close()
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
