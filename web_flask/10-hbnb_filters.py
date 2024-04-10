f7797272955c95ca5a8884727bb88bd27f1faa26
Omar J <omarj819@gmail.com>
2024-04-10 15:35:39 +0300

    A

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,40 +1,30 @@
 #!/usr/bin/python3
 """
-Script to start a Flask web application
+    Script that start a Flask web application
+    listening on 0.0.0.0 port 5000.
 """
-
 from flask import Flask, render_template # type: ignore
-from models import storage
-from models.state import State
-from models.city import City
 from models.amenity import Amenity
-
+from models.state import State
+from models import storage
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def hbnb_filters():
-    """
-    Route to display a HTML page like 6-index.html
-    """
-    states = storage.all(State).values()
-    cities = storage.all(City).values()
-    amenities = storage.all(Amenity).values()
-    return render_template(
-        '10-hbnb_filters.html',
-        states=states,
-        cities=cities,
-        amenities=amenities
-    )
-
-
 @app.teardown_appcontext
-def teardown(exception):
-    """
-    Teardown method to remove the current SQLAlchemy Session
-    """
+def teardown(self):
+    """ Closes sessions. """
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def states_id(id=None):
+    """ List all states and cities. """
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
+    return render_template("10-hbnb_filters.html", states=states,
+                           amenities=amenities)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
