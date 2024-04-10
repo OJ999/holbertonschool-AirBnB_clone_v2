2d62e9bd6efe8c98dbd268ab35b5494b461882dc
Omar J <omarj819@gmail.com>
2024-04-10 14:57:28 +0300

    A

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
 """
-Script to start a Flask web application
+Simple module that starts a Flask web application
+Starting to display formatted text and conditional message
+Addind basic template
 """
-
-from flask import Flask, render_template # type: ignore
+from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """
-    Route to display a HTML page with a list of all State objects
-    """
-    states = storage.all("State").values()
-    return render_template('7-states_list.html', states=states)
-
-
 @app.teardown_appcontext
-def teardown(exception):
+def handle_appcontext(self):
     """
-    Teardown method to remove the current SQLAlchemy Session
+    Method for handling app context
     """
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/states_list', strict_slashes=False)
+def list_states():
+    """
+    Displays 7-states_list html page
+    """
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    """ app listening on host 0.0.0.0 and port 5000 """
+    app.run(host='0.0.0.0', port='5000')
