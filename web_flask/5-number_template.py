07456a212e1b629ff803930421242a282c1a349d
Omar J <omarj819@gmail.com>
2024-04-10 14:49:44 +0300

    A

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,62 +1,52 @@
 #!/usr/bin/python3
 """
-Script to start a Flask web application
+A script that starts a Flask web application
 """
-
-from flask import Flask, render_template # type: ignore
-from urllib.parse import unquote
+from flask import Flask, abort # type: ignore
+from sys import argv
+from flask import render_template # type: ignore
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """
-    Route to display 'Hello HBNB!'
-    """
-    return 'Hello HBNB!'
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    """
-    Route to display 'HBNB'
-    """
-    return 'HBNB'
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def display_c_text(text):
-    """
-    Route to display 'C ', followed by the value of the text variable
-    """
-    return 'C {}'.format(unquote(text).replace('_', ' '))
+@app.route("/c/<text>", strict_slashes=False)
+def display_c(text):
+    return f"C {text.replace('_', ' ')}"
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def display_python_text(text):
-    """
-    Route to display 'Python ', followed by the value of the text variable
-    """
-    return 'Python {}'.format(unquote(text).replace('_', ' '))
+@app.route("/python", defaults={'text': "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def display_py(text):
+    return f"Python {text.replace('_', ' ')}"
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def display_number(n):
-    """
-    Route to display 'n is a number' only if n is an integer
-    """
-    return '{} is a number'.format(n)
+@app.route("/number/<n>", strict_slashes=False)
+def display_num(n):
+    if n.isdigit():
+        n = int(n)
+        return f"{n} is a number"
+    if not n.isdigit():
+        abort(404)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def display_number_template(n):
-    """
-    Route to display a HTML page only if n is an integer
-    """
-    return render_template('5-number.html', n=n)
+@app.route("/number_template/<n>", strict_slashes=False)
+def number_template(n):
+    if n.isdigit():
+        n = int(n)
+        return render_template('5-number.html', title='HBNB', n=n)
+    if not n.isdigit():
+        abort(404)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
