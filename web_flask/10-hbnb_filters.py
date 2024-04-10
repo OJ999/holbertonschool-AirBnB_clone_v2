3928739757b2327c9dab9a7bc481e48a8e18e17e
Omar J <omarj819@gmail.com>
2024-04-10 15:11:42 +0300

    A

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,31 +1,40 @@
 #!/usr/bin/python3
-"""Start web application with two routings
+"""
+Script to start a Flask web application
 """
 
+from flask import Flask, render_template # type: ignore
 from models import storage
 from models.state import State
+from models.city import City
 from models.amenity import Amenity
-from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters')
+@app.route('/hbnb_filters', strict_slashes=False)
 def hbnb_filters():
-    """Render template with states
     """
-    path = '10-hbnb_filters.html'
-    states = storage.all(State)
-    amenities = storage.all(Amenity)
-    return render_template(path, states=states, amenities=amenities)
+    Route to display a HTML page like 6-index.html
+    """
+    states = storage.all(State).values()
+    cities = storage.all(City).values()
+    amenities = storage.all(Amenity).values()
+    return render_template(
+        '10-hbnb_filters.html',
+        states=states,
+        cities=cities,
+        amenities=amenities
+    )
 
 
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """Clean-up session
+def teardown(exception):
+    """
+    Teardown method to remove the current SQLAlchemy Session
     """
     storage.close()
 
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
