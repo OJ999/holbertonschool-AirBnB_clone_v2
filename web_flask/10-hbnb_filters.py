40a6f2555d27d4272b654e3c189e9dda2b4a8954
Omar J <omarj819@gmail.com>
2024-04-10 17:08:55 +0300

    A

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,40 +1,28 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
-from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from os import environ
+"""
+Starts a Flask web application to display filters for states, cities, and amenities.
+"""
+
 from flask import Flask, render_template
+from models import storage
+
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
-def close_db(error):
-    """ Remove the current SQLAlchemy Session """
+def teardown_appcontext(exception):
+    """Remove the current SQLAlchemy Session."""
     storage.close()
 
 
 @app.route('/hbnb_filters', strict_slashes=False)
-def hbnb_filter():
-    """ HBNB filters """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    st_ct = []
-
-    for state in states:
-        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
-
-    amenities = storage.all(Amenity).values()
-    amenities = sorted(amenities, key=lambda k: k.name)
-
-    return render_template('10-hbnb_filters.html',
-                           states=st_ct,
-                           amenities=amenities)
+def hbnb_filters():
+    """Display a HTML page with filters for states, cities, and amenities."""
+    states = sorted(list(storage.all("State").values()), key=lambda state: state.name)
+    cities = sorted(list(storage.all("City").values()), key=lambda city: city.name)
+    amenities = sorted(list(storage.all("Amenity").values()), key=lambda amenity: amenity.name)
+    return render_template('10-hbnb_filters.html', states=states, cities=cities, amenities=amenities)
 
 
-if __name__ == "__main__":
-    """ Main Function """
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
